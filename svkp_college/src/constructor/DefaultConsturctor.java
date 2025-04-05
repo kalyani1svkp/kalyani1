f5fa7a9ee298a4cced8f3c93600071551de24560
HI <HI@DESKTOP-I9H8KJC>
2025-04-04 21:49:19 -1200

    commit 3

---
+++ svkp_college/src/constructor/DefaultConsturctor.java
@@ -0,0 +1,33 @@
+package constructor;
+
+	class Myclass1
+	{
+		String color;
+		int cost;
+		String brand;
+		
+		
+		//default constructor
+		Myclass1()
+		{
+			color="Pink";
+			cost=2000000;
+			brand="BMW";
+		}
+	}
+
+
+
+	public class DefaultConsturctor {
+
+		public static void main(String[] args) {
+			Myclass1 obj=new Myclass1();
+			System.out.println(obj.color + " "+ obj.cost+ " " +obj.brand);
+			System.out.println(obj.color + " "+ obj.cost+ " " +obj.brand);
+			
+
+		}
+
+	}
+
+
