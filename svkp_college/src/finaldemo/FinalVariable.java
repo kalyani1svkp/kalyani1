5bc627ad6437c013845c3eeb5f072edf186d85ab
HI <HI@DESKTOP-I9H8KJC>
2025-04-05 15:55:30 -1200

    commit 6

---
+++ svkp_college/src/finaldemo/FinalVariable.java
@@ -0,0 +1,11 @@
+package finaldemo;
+
+public class FinalVariable {
+	public static void main(String[] args) {
+		int  mark_scored_leela=50;
+		mark_scored_leela=80; //final variable can't be changed
+		System.out.println(mark_scored_leela);
+
+	}
+
+}
