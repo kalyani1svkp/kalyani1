826cebdb066f6e109eaeb19cca9334a65ed3c4b7
HI <HI@DESKTOP-I9H8KJC>
2025-04-05 15:27:11 -1200

    commit5

---
+++ svkp_college/src/manager_rules/KIA_Rules.java
@@ -0,0 +1,11 @@
+package manager_rules;
+
+
+public class KIA_Rules {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ svkp_college/src/sr_developer/KIA_Internal.java
@@ -0,0 +1,15 @@
+package sr_developer;
+import jr_developer.*;
+public class KIA_Internal {
+
+	public static void main(String[] args) {
+		
+		KIA_AirBags obj=new KIA_AirBags();
+		obj.disp_AirBags();
+		
+//		 KIA_Internal obj1=new  KIA_Internal();
+//		obj1.disp_Speed_limit();
+
+	}
+
+}
