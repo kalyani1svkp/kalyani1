29339d1734f5330253bb8e284ae1ed110f5e166e
HI <HI@DESKTOP-I9H8KJC>
2025-04-05 16:10:14 -1200

    commit 6

---
+++ svkp_college/src/oops/MethodOverloadingImp.java
@@ -0,0 +1,28 @@
+package oops;
+
+public class MethodOverloadingImp {
+	public int add(int a,int b) //2 para
+	{
+		return a+b;
+	}
+	public int add(int a, int b , int c) //3 para
+	{
+		return a+b+c;
+	}
+	public double add(int a, double b)
+	{
+		return a+b;
+	}
+	
+	public static void main(String[] args) {
+		MethodOverloadingImp obj= new MethodOverloadingImp();
+		System.out.println(obj.add(4, 8));
+		System.out.println(obj.add(2, 2.5));
+		System.out.println(obj.add(4, 8,7));
+		
+		
+
+	}
+
+}
+
